dfd247d24b1ebc8c56b96d0649cce648f677d1f8
JorgeArturo008 <87247686+JorgeArturo008@users.noreply.github.com>
2021-08-25 18:50:51 -0600

    inicio
    se actualiza

---
+++ ExamenFinal/src/examenfinal/DatosdelRegistro.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenfinal;
+
+/**
+ *
+ * @author 50684
+ */
+public class DatosdelRegistro {
+
+    
+    
+    String nombre;
+    String Pass;
+
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPass() {
+        return Pass;
+    }
+
+    public void setPass(String Pass) {
+        this.Pass = Pass;
+    }
+
+    @Override
+    public String toString() {
+        return "DatosdelRegistro{"  + ", nombre=" + nombre + ", Pass=" + Pass + '}';
+    }
+
+}
